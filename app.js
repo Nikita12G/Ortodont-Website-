8c50bb6d9b14e7568b363ffef22d1fab569383ec
Nikita12G <gulyi08@mail.ru>
2024-04-28 17:55:04 +0700

    correctTitleAndLinkClick

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    // Обработчик клика по ссылкам с атрибутом data-scroll
-    $('a[data-scroll]').click(function(e) {
-        e.preventDefault();
+    // Обработчик для клика и касания по ссылкам с атрибутом data-scroll
+    $('a[data-scroll]').on('click touchstart', function(e) {
+        e.preventDefault(); // Предотвращение стандартного поведения ссылки
         var targetId = $(this).attr('data-scroll');
         var targetOffset = $(targetId).offset().top;
 
+        // Анимация прокрутки
         $('html, body').animate({
             scrollTop: targetOffset
         }, 1000);
