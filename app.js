9e12e2b98d90a5b1a3c95cbf3ddcc4a6c105ddc1
Nikita12G <gulyi08@mail.ru>
2024-04-29 16:57:44 +0700

    updateLightboxLogic

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-    // Обработчик для клика по ссылкам с атрибутом data-scroll
-    $('a[data-scroll]').on('click', function(e) {
-        e.preventDefault(); // Предотвращение стандартного поведения ссылки
+    // Handler for click and touch events on links with the 'data-scroll' attribute
+    $('a[data-scroll]').on('click touchstart', function(e) {
+        e.preventDefault(); // Prevent the default link behavior
         var targetId = $(this).attr('data-scroll');
         var targetOffset = $(targetId).offset().top;
 
-        // Анимация прокрутки
+        // Scrolling animation
         $('html, body').animate({
             scrollTop: targetOffset
         }, 1000);
+    });
+
+    // Lightbox options configuration
+    lightbox.option({
+        'resizeDuration': 600,
+        'wrapAround': true,
+        'alwaysShowNavOnTouchDevices' : true,
+        'disableScrolling' : true
     });
 });
